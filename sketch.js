0222a2178d793ba8b5b2a9ad1267a6c4ae86368f
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2020-12-24 13:02:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+//load matter.js libraries
+
+
+
+
+
+
+function preload()
+{
+	//load Image
+
+
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	//package sprite(width/2) and addImage and scale
+	
+
+
+	//helicopter sprite(width/2) and addImage and scale
+	
+
+
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
